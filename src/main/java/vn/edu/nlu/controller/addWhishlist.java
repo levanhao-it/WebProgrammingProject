3439203b59f58f9eace8bd3b306d46fee05aec0b
huynhhuy0907 <18130095@st.hcmuaf.edu.vn>
2020-12-29 13:54:46 +0700

    whishlist

---
+++ src/main/java/vn/edu/nlu/controller/addWhishlist.java
@@ -0,0 +1,37 @@
+package vn.edu.nlu.controller;
+
+import vn.edu.nlu.beans.Product;
+import vn.edu.nlu.entity.ProductEntity;
+import vn.edu.nlu.model.Whishlist;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/addWhishlist")
+
+public class addWhishlist extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        if(id==null) response.sendRedirect("Home");
+        ProductEntity pe = new ProductEntity();
+        Product p = pe.getById(id);
+        if(p==null){
+            response.sendRedirect("Home");
+            return;
+        }
+        HttpSession session =request.getSession();
+        Whishlist wl = Whishlist.getWhishlist(session);
+            wl.put(p);
+
+    }
+
+}
---
+++ src/main/java/vn/edu/nlu/model/Whishlist.java
@@ -0,0 +1,54 @@
+package vn.edu.nlu.model;
+
+import vn.edu.nlu.beans.Product;
+
+import javax.servlet.http.HttpSession;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Whishlist implements Serializable {
+    Map<String, Product> data= new HashMap<>();
+
+    public Whishlist(){
+
+    }
+    public static Whishlist getWhishlist(HttpSession session) {
+        return session.getAttribute("whishlist")==null
+                ? new Whishlist()
+                : (Whishlist) session.getAttribute("whishlist");
+    }
+
+    public Product get(int id){
+        return data.get(id);
+    }
+    public void put(Product p){
+        if(p==null) return ;
+        if(data.containsKey(p.getId())){
+            data.get(p.getId()).add();
+            return;
+        }
+        p.setQuantity(1);
+        data.put(p.getId(),p);
+    }
+    public void price(int id){
+        data.get(id);
+    }
+    public void update(int id,int quantity){
+        if(quantity<0) return;
+        if(data.containsKey(id)) data.get(id).setQuantity(quantity);
+
+    }
+    public void detail(){
+    }
+    public void add(){
+
+    }
+    public void remove(int id){
+        data.remove(id);
+    }
+    public Collection<Product> getData(){
+        return data.values();
+    }
+}

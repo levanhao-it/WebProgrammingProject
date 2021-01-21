c0cac83b261beb5bb0dd9f96c551b608ca8920bd
lehuuhuy29 <lehuuhuy2902@gmail.com>
2021-01-21 11:35:27 +0700

    edit

--- src/main/java/vn/edu/nlu/controller/Home.java
+++ src/main/java/vn/edu/nlu/controller/Home.java
@@ -18,6 +18,25 @@ public class Home extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String index = request.getParameter("index");
+        int indexPage = 0;
+        if(index == null){
+            indexPage = 1;
+        }else{
+            indexPage = Integer.parseInt(index);
+        }
+        ProductEntity pe = new ProductEntity();
+        int countProduct = pe.getAll().size();
+        int pageSize = 24;
+        int endPage;
+        if(countProduct % pageSize ==0)
+            endPage = countProduct/pageSize;
+        else endPage = (countProduct/pageSize) +1;
+        int beginPage = indexPage*pageSize - (pageSize-1) - 1;
+        Collection<Product> data = pe.getAllProducts(beginPage,pageSize);
+        request.setAttribute("index", indexPage);
+        request.setAttribute("data", data);
+        request.setAttribute("endPage", endPage);
         request.getRequestDispatcher("index.jsp").forward(request,response);
 
     }

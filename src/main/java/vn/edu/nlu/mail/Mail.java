f14b38e71f528c347619db53d1ddd7f4ae2e388e
huynhhuy0907 <18130095@st.hcmuaf.edu.vn>
2021-01-24 15:55:02 +0700

    edit mail

--- src/main/java/vn/edu/nlu/mail/Mail.java
+++ src/main/java/vn/edu/nlu/mail/Mail.java
@@ -3,40 +3,51 @@ package vn.edu.nlu.mail;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Mail {
-    public static void main(String[] args) {
+    public static void sendMail(String recepient) throws Exception {
         Properties pro = new Properties();
-        pro.put("mail.smpt.host","smtp.gmail.com");
-        pro.put("mail.smtp.port","587");
-        pro.put("mail.smtp.auth","true");
-        pro.put("mail.smtp.starttls.enable","true");
+        pro.put("mail.smtp.auth", "true");
+        pro.put("mail.smtp.starttls.enable", "true");
+        pro.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        pro.put("mail.smtp.host", "smtp.gmail.com");
+        pro.put("mail.smtp.port", "587");
 
-        String username = "haole7301@gmail.com";
-        String password = "ioowcbwekyybzurx";
+        String username = "huynhhuy90700@gmail.com";
+        String password = "tkkmysjbhoyrfngf";
 
-        Session session = Session.getInstance(pro,
-                new Authenticator() {
-                    @Override
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                }
-        );
-        Message message = new MimeMessage(session);
-        try{
-            message.setFrom(new InternetAddress("haole7301@gmail.com","Hào Lê"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse("huynhhuy90700@gmail.com"));
-            message.setSubject("Test");
-            message.setText("abc");
-            Transport.send(message);
-            System.out.println("Done");
-        } catch (MessagingException | UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        Session session = Session.getInstance(pro, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+        Message message = prepareMessage(session, username,recepient);
+        Transport.send(message);
+        System.out.println("Gửi thành công");
+
+
     }
 
+    private static Message prepareMessage(Session session, String username, String recepient) {
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject("Test");
+            message.setText("heyyyyyy");
+            return message;
+        }catch(Exception e){
+            Logger.getLogger(Mail.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Mail.sendMail("huynhhuy90700@gmail.com");
+
+    }
 }
